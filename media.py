77c5469fff5325f35baaabe7b2d41c9b33ba5786
Carla MT <girlcodechronicles@gmail.com>
2017-12-04 19:56:22 -0500

    Add files via upload

---
+++ media.py
@@ -0,0 +1,34 @@
+"""
+Module to display movie object, attributes and instances
+"""
+
+import webbrowser
+
+
+class Movie():
+
+    """
+    Class object stores movie related information
+    """
+
+def_init__(self, movie_title,
+           movie_storyline, poster_image, trailer_youtube):
+        """
+        Initialize instances of class Movie
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube:string
+        """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+
+# Opens the web browser
+def show_trailer(self):
+            """Initializing instance for opening the youtube video
+            :return: webbrowser to play trailer
+            """
+            webbrowser.open(self.trailer_youtube_url)
